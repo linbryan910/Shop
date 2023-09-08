7a2014b613575cde92608067c635404ed1e68c65
Bryan Lin <brylinamln@gmail.com>
2023-09-08 14:39:33 -0700

    Added a filter for the item name/category when customer browses store

--- Shop/Pages/Customer/Browse/Index.cshtml.cs
+++ Shop/Pages/Customer/Browse/Index.cshtml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Shop.Data;
 using Shop.Models;
 
@@ -24,12 +26,36 @@ namespace Shop.Pages.Customer.Browse
         [BindProperty(SupportsGet = true)]
         public int CustId { get; set; }
 
+        public SelectList Categories { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchCategory { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; } = default!;
+
         public async Task OnGetAsync()
         {
-            if (_context.Inventory != null)
-            {
-                Item = await _context.Inventory.ToListAsync();
+            // Gets all categories for items
+            var categories = from i in _context.Inventory
+                             orderby i.Category
+                             select i.Category;
+
+            Categories = new SelectList(await categories.Distinct().ToListAsync());
+
+            // Gets items (filters by category/name if inputted)
+            var item = from i in _context.Inventory
+                       select i;
+
+            if (!SearchCategory.IsNullOrEmpty()) {
+                item = item.Where(i => i.Category == SearchCategory);
             }
+
+            if (!SearchString.IsNullOrEmpty()) {
+                item = item.Where(i => i.Name.Contains(SearchString));
+            }
+
+            Item = await item.ToListAsync();
         }
     }
 }
